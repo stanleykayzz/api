515d4ab963745d8156a4eb58c9df0ed35a19f926
Ondzounga <ondzoungabruce@hotmail.fr>
2015-05-02 01:08:13 +0200

    création de publication opérationnel , pour chaque publication créé on crée un commentaire dans la table comment qui possède l'id_posts de la publication créée. on peut aussi supprimer et modifier les publications mais les commentaires ne sont pas encore pris en compte

--- Publication/post.php
+++ Publication/post.php
@@ -2,14 +2,13 @@
 
 echo "<meta http-equiv=\"content-type\" content=\"text/html; charset=utf-8\" />";
 
-
 require_once("C:/wamp/www/Projet_API/Comments/comment.php");
 
-class post
+class post extends comment
 {
 	private $user;
 	private $idPost;
-	private $description;
+	private $description;//on passe un objet commentaire en parametre
 
 	//je crée un constructeur pour les publications
 	public function __construct($id_user,$des)
@@ -21,34 +20,44 @@ class post
 
 	public function createPost($utilisateur,$texte)
 	{
+
+		$this->description = $texte;
+		$this->users = $utilisateur;
+
 		/*Création d’une publication à partir des informations fournies.
 Un post contient une description qui est en fait un commentaire.
 Donc lors de la création du post il doit être possible de créer un commentaire
 - Il est possible d’associer (tagguer) des amis à une publication*/
 	
-
 		try
 			{	
 
-				//ON établi une connexion avec la base de données
+			//ON établi une connexion avec la base de données
 			$pdo = new PDO("mysql:host=localhost;dbname=projetapi", 'root','');
-	        echo "<br/> ça passe, tu es connecté à la bdd ";
+	        echo "<br/> ça passe, tu es connecté à la bdd ";       
 
-	        //On fait un insert du commentaire dans la table commentaire
+			 //On fait un insert du texte dans la table publication
 	        //on crée une requete sql
-	        $query = "INSERT INTO post VALUES(:id_posts, :texte,:id_users)";
-
-			//la description d'un post est un commentaire , on crée donc un commentaire
-			$theComment = new comment( $this->idPost, $this->description);	     
+	        $query = "INSERT INTO post VALUES(:id_posts, :texte,:id_users)"; 
 
 			//on récupère le dernier id de post et on l'augmente à chaque post
-	        $this->idPost = $pdo->lastInsertId();
+	        //$this->idPost = $pdo->lastInsertId();
 
-	        echo " <br/><br/> <h3>la dernière valeur est ".$this->idPost."</h3>";
+	        //$lastIdpost;
+
+	        $statement = $pdo->query("SELECT * FROM  `post`");
+    		while($row =$statement->fetch(PDO::FETCH_ASSOC) ){
+	        $lastIdpost=$row["id_posts"];}
+
+	        //On fait un insert du commentaire dans la table commentaire
+			//la description d'un post est un commentaire , on crée donc un commentaire
+			//dont l'idPost est le post actuel et le texte celui du commentaire
+			$theComment = new comment( $lastIdpost++, $texte);	
+			$theComment->createComment($lastIdpost++, $texte);
 
 	         $req = $pdo->prepare($query);
 	         $req->execute(array(
-	         	':id_posts' => $this->idPost++,
+	         	':id_posts' => $lastIdpost++,
 	         	':texte' => $this->description,
 	         	':id_users' => $this->user
 	         ));
@@ -118,15 +127,13 @@ supprimer tous les éléments associés à cette publication.*/
 	        echo "ça passe pas";
 	}
 	}
-
-
 }
 
 //$publicationDeMoi = new post(2,2,"Quand on fini est projet de php on est léger ;)");
 //$publi = new post(1,10,"faut reconnaitre que le java est quand même plus long et plus chiant que le php -_-");
-$publi = new post(2,"hello les gens!!!! :)");
-$publi->deletePost(8);
-$publi->alterPost(5,"Bon moi je vais me coucher, à plus");
+$publi = new post(1," le bouch-money");
+//$publi->deletePost(8);
+//$publi->alterPost(5,"Bon moi je vais me coucher, à plus");
 
 
 
