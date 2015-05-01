e11a752b22459900b7c0319a30ed94c4686dd6f8
Ondzounga <ondzoungabruce@hotmail.fr>
2015-05-01 21:29:54 +0200

    les fonctions de création , de suppression et de modification des commentaires ont été ajouté dans le même fichier (comment.php) et sont toutes opérationnelles

--- Comments/comment.php
+++ Comments/comment.php
@@ -1,27 +1,24 @@
 <?php
-	require_once("create_comment.php");
-	require_once("delete_comment.php");
-	
+
+
 class comment
 {
 	private $post;
 	private $comment;
 	private $id_com;
 
-	public function __construct($Publication,$commentaire)
+	public function __construct($publication,$commentaire)
 	{
 		$this->comment = $commentaire;
-		$this->post = $Publication;
-		$this->createComment($Publication,$commentaire);
+		$this->post = $publication;
 	}
 
-
-	function alterComment($post, $text)
+	//permet de créer un commentaire dans la bdd
+	public function createComment($post, $text)
 	{
 		try
 			{	
-
-				//ON établi une connexion avec la base de données
+			//ON établi une connexion avec la base de données
 			$pdo = new PDO("mysql:host=localhost;dbname=projetapi", 'root','');
 			echo "ca passe";
 
@@ -30,13 +27,65 @@ class comment
 	        //On ajoute des variable à la suite
 	         $this->id_com = $pdo->lastInsertId();
 
-	         echo "xxxxxxxxxxx  ".$this->id_com;
-
 	         $req = $pdo->prepare($query);
 	         $req->execute(array(
 	         	':id_comms' => $this->id_com,
 	         	':texte' => $text,
 	         	':id_posts' => $post
+	         ));
+
+	         unset($req);
+
+	}
+	catch (Exception $e)
+	{
+	        die('Erreur : ' . $e->getMessage());
+	        echo "ça passe pas";
+	}		
+
+	}
+
+	public function alterComment($idcom,$modifText) 
+	{
+
+			try
+			{		
+
+				$pdo = new PDO("mysql:host=localhost;dbname=projetapi", 'root','');
+				echo " alter";
+
+				//on met à jour 
+				$updateQuery = "UPDATE comment SET texte = :texte WHERE id_comms = :id_com";
+
+				$req = $pdo->prepare($updateQuery);
+	 	        $req->execute(array(':texte' => $modifText,':id_com' => $idcom ));
+
+	 	    	unset($req);
+	 	    }
+	catch (Exception $e)
+	{
+	        die('Erreur : ' . $e->getMessage());
+	        echo "ça passe pas";
+	}	
+
+	}
+
+	//permet de supprimer les commentaires dont l'id est passé en paramètre
+	public function deleteComment($idComm)
+	{
+		try
+			{	
+
+			//ON établi une connexion avec la base de données
+			$pdo = new PDO("mysql:host=localhost;dbname=projetapi", 'root','');
+			echo "ca passe";
+  
+	        $deleteQuery = "DELETE FROM comment WHERE id_comms = :id_comms";
+				$state = $pdo->prepare($deleteQuery);
+
+	         $req = $pdo->prepare($deleteQuery);
+	         $req->execute(array(
+	         	':id_comms' => $idComm
 	         ));
 
 	}
@@ -47,6 +96,7 @@ class comment
 	}		
 
 	}
+
 }
 
 ?>
