1ff2773b3af25615ab3fcee589dd2b4612cb648b
Xavier Negre <blackbomba32@hotmail.fr>
2015-05-01 16:07:38 +0200

    first modif connexion.php

--- Users/connexion.php
+++ Users/connexion.php
@@ -2,17 +2,52 @@
 	require_once("cls-u_1n_130.php");
 	session_start();
 	
+	$mail = $_GET["mail"];
+	$passwd = $_GET["passwd"];
+	
 	$user = new Users();
 	
-	$user->setMail($_SESSION["mail"]);
-	$user->setPassword($_SESSION["passwd"]);
-	$user->setPseudo($_SESSION["pseudo"]);
-	$user->setDateOfBirth($_SESSION["date_birth"]);
-	
-	function connexion(String $pseudo, String $passwd) {
-		$pdo = new PDO("mysql:host=localhost;dbname=db_ap", "root", "");
-		
-		$params = array(":id"=>$_SESSION["id"], ":psw"=>$user->getPassword());
+	function connexion(String $mail, String $passwd) {
+		if(!empty($mail) and !empty($passwd) and !empty($_GET["send"])) {
+			if(!($pdo = new PDO("mysql:host=localhost;dbname=db_ap", "root", "")) {
+				$output = array(
+					"code"=>5,
+					"result"=>"Internal server error!",
+					"infos"=>array(
+						"query"=>"SELECT DB = ".$dbname." WITH id = ".$dbid." AND psw = ".$dbpsw." AND host = ".$host
+					)
+				);
+				return json_encode($ouput);
+			}
+			
+			$params = array(
+				":id"=>$_SESSION["id"],
+				":mail"=>$mail,
+				":psw"=>$passwd
+			);
+			
+			$select = "SELECT * FROM user WHERE mail = :mail AND password = :psw";
+			$state = $pdo->prepare($select);
+			
+			if($state and $state->execute($params)) {
+				$lignes = $state->fetchAll();
+				if(count($lignes) > 1) {
+				
+				}
+			} else {
+				$ouput = array(
+					"code"=>5,
+					"result"=>"Internal server error!";
+				);
+				return json_encode($output);
+			}
+		} else {
+			$output = array(
+				"code"=>1
+				"result"=>"Missing required parameter(s)!"
+			);
+			return json_encode($output);
+		}
 		
 		$state = $pdo->query("SELECT * FROM users WHERE pseudo = :id AND password = :psw");
 		
