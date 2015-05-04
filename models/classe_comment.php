767791e9db8f189a50d38f69b3653ab5449605e3
Ondzounga <ondzoungabruce@hotmail.fr>
2015-05-04 18:05:39 +0200

    rajout de l'id utilisateur dans la création d'un comentaire

--- models/classe_comment.php
+++ models/classe_comment.php
@@ -6,23 +6,29 @@ class comment
 	private $post;
 	private $comment;
 	private $id_com;
+	private $id_user;
 
-	public function __construct($publication,$commentaire)
+	public function __construct()
 	{
-		$this->comment = $commentaire;
-		$this->post = $publication;
+		$this->comment = "";
+		$this->post = 0;
+		$this->id_user = 0;
 	}
 
 	//permet de créer un commentaire dans la bdd
-	public function createComment($post, $text)
+	public function createComment($post, $text, $user)
 	{
+
+		$this->comment = $text;
+		$this->post = $post;
+		$this->id_user = $user;
 		try
 			{	
 			//ON établi une connexion avec la base de données
 			$pdo = new PDO("mysql:host=localhost;dbname=projetapi", 'root','');
 			echo "ca passe";
 
-	        $query = "INSERT INTO comment VALUES(:id_comms,:texte, :id_posts)";    
+	        $query = "INSERT INTO comment VALUES(:id_comms,:texte, :id_posts, :id_users)";    
 
 	        //On ajoute des variable à la suite
 	         $this->id_com = $pdo->lastInsertId();
@@ -31,7 +37,8 @@ class comment
 	         $req->execute(array(
 	         	':id_comms' => $this->id_com,
 	         	':texte' => $text,
-	         	':id_posts' => $post
+	         	':id_posts' => $post,
+	         	':id_users' => $user
 	         ));
 
 	         unset($req);
@@ -47,7 +54,7 @@ class comment
 
 	public function alterComment($idcom,$modifText) 
 	{
-
+		$this->comment = $modifText;
 			try
 			{		
 
@@ -96,7 +103,13 @@ class comment
 	}		
 
 	}
-
 }
+
+
+$v = new comment();
+//$v->createComment(1,"je commente la première publication de stanleyKayzz",2);
+//$v->createComment(1,"c'est bien , continue comme ca alors :p",1);
+//$v->createComment(1," blala",1);
+//$v->alterComment(1,"je viens de modifier mon commentaire",1);
 
 ?>
