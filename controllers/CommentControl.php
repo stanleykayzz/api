e1b6cc31c32145d900823a0681c3d5897179c0ac
Ondzounga <ondzoungabruce@hotmail.fr>
2015-05-04 02:01:59 +0200

    commentControl mis à jour

--- controllers/CommentControl.php
+++ controllers/CommentControl.php
@@ -3,7 +3,28 @@
 Class CommentControl
 {
 	$private $params_;
-	
+
+	public function createComment()
+	{
+		$com = new comment();
+
+		return json_encode($com->createComment($this->params_["id_posts"], $this->params_["texte"]));
+	}
+
+	public function alterComment()
+	{
+		$com = new comment();
+
+		return json_encode($com->createComment($this->params_["id_comms"], $this->params_["texte"]));
+	}
+
+	public function deleteComment()
+	{
+		$com = new comment();
+
+		return json_encode($com->createComment($this->params_["id_comms"]));
+	}
+
 }
 
 
