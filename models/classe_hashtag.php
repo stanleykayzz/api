eb406ca2523a3867af4b45f6acf290f803c01caf
Ondzounga <ondzoungabruce@hotmail.fr>
2015-05-04 23:02:15 +0200

    ajout de la classe hashtag qui permet de rajouter des mot clé dans la bdd

--- models/classe_hashtag.php
+++ models/classe_hashtag.php
@@ -1,5 +1,39 @@
 ﻿<?php
-	class Hashtag {
-		
+	class hashtag {
+
+		private $hashtag;
+		private $motCle;
+
+		public function __construct()
+		{
+			$this->hashtag= 0;
+			$this->motCle= "";
+		}
+
+		public function createHashtag($mot)
+		{
+		$this->motCle = $mot;
+			try
+			{	
+			//ON établi une connexion avec la base de données
+			$pdo = new PDO("mysql:host=localhost;dbname=projetapi", 'root','');
+	        $query = "INSERT INTO hashtag VALUES(:id_hashtag, :mot)";    
+
+	        //On recupere la derniere valeur de la table hashtag
+	         $this->this->hashtag = $pdo->lastInsertId();
+
+	        $req = $pdo->prepare($query);
+	        $req->execute(array(
+	         	':id_hashtag' => $this->hashtag,
+	         	':mot' => $mot
+	         ));
+	         }
+	catch (Exception $e)
+	{
+	        die('Erreur : ' . $e->getMessage());
+	        echo "ça passe pas";
+	}		
+	}
+
 	}
 ?>
